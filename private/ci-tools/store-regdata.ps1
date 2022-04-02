c405e88ef81a3ded75a493a559ca2ec359eb82c3
danijeljw-rpc <danijel@repasscloud.com>
2022-04-02 17:59:31 +1100

    registry scan included for installer

---
+++ private/ci-tools/store-regdata.ps1
@@ -0,0 +1,9 @@
+[System.Array]$hklmPaths = @(
+    "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+)
+
+foreach ($Path in $hklmPaths)
+{
+    Get-ChildItem -Path $Path | Get-ItemProperty | Where-Object -FilterScript {$null -notlike $_.DisplayName} | Select-Object -Property Publisher,DisplayName,DisplayVersion | Export-Csv -Path C:\Projects\libsfw2\public\installed_report.csv -NoTypeInformation
+} 
