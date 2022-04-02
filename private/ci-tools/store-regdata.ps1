8359562c4bd92e9cb5f1332a7811dfcebc487352
danijeljw-rpc <danijel@repasscloud.com>
2022-04-02 19:14:10 +1100

    purgatory

--- private/ci-tools/store-regdata.ps1
+++ private/ci-tools/store-regdata.ps1
@@ -8,7 +8,8 @@ foreach ($Path in $hklmPaths)
     Get-ChildItem -Path $Path | Get-ItemProperty | Where-Object -FilterScript {$null -notlike $_.DisplayName} | Select-Object -Property Publisher,DisplayName,DisplayVersion | Export-Csv -Path C:\Projects\libsfw2\public\installed_report.csv -NoTypeInformation
 } 
 
-
+$Count = (Import-Csv -Path C:\Projects\libsfw2\public\installed_report.csv).Count
+Write-Output "installed application in store-regdata is: ${Count}"
 
 
 
