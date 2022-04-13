6e5432c1f5032104f185e334305e2f0e3703781c
danijeljw-rpc <danijel@repasscloud.com>
2022-04-14 00:16:18 +1000

    clean

--- pull.ps1
+++ pull.ps1
@@ -1,1 +1,2 @@
 git subtree pull --prefix=libsfw-ps git@github.com:repasscloud/libsfw-ps.git main --squash
+git subtree pull --prefix=data/corelib git@github.com:repasscloud/libsfw-corelib.git main --squash
