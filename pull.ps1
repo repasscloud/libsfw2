6e6ac3bf137ec206ce6608e3ee408f516d1810f8
danijeljw-rpc <danijel@repasscloud.com>
2022-04-28 17:29:26 +1000

    update pull script

--- pull.ps1
+++ pull.ps1
@@ -1,2 +1,3 @@
 git subtree pull --prefix=libsfw-ps git@github.com:repasscloud/libsfw-ps.git main --squash
 git subtree pull --prefix=data/corelib git@github.com:repasscloud/libsfw-corelib.git main --squash
+git push
